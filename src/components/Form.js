77358a4cacfdbad163d0b0a1a1957b32bec9ae07
Krissy Macdonald <krissymacdonald5@gmail.com>
2022-12-07 00:13:34 -0500

    intialized

---
+++ src/components/Form.js
@@ -0,0 +1,14 @@
+
+
+const Form = () => {
+    
+    return (
+        <div>
+            <h1>Get a Giphy</h1>
+            <h2>Powered By Giphy</h2>
+            <input type='submit' value='RANDOM' />
+        </div>
+    )
+}
+
+export default Form;
